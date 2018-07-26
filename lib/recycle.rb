84e92df7e50d181ce3a4e2e55a3c66ad7d312b93
Rebecca Souza <rebeccacsouza@gmail.com>
2018-07-26 13:55:23 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -18,12 +18,21 @@
 #1. Define a method `address`, that takes a hash as an argument.
 #This method should return the value of the address key in the hash.
 
+def address(hash = {})
+  return hash[address]
+end
+
 #2. Define a method called `borough` that takes a hash as an argument.
 # This method should return the value of the borough key in the hash.
+
+def borough(hash2 = {})
+  return hash2[borough]
+end
 
 #3. Define a method called `all_keys` that takes a hash as an argument.
 #This method should print out each key in the hash. 
 #You should use the `each_key` method to iterate over your hash and print out each key.
+
 
 #4. Define a method called `all_data` that takes a hash as an argument.
 #This method should print out each value in the hash.
